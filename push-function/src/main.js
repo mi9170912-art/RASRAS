6e0ff8f02b388ad586bdeb404dca0fbd18609150
mi9170912-art <mi9170912@gmail.com>
2026-09-22 15:31:55 +0300

    Update Appwrite endpoint and project ID configuration

--- push-function/src/main.js
+++ push-function/src/main.js
@@ -38,8 +38,8 @@ module.exports = async ({ req, res, log, error }) => {
     ensureFirebase(log);
 
     const client = new Client()
-      .setEndpoint(process.env.APPWRITE_ENDPOINT)
-      .setProject(process.env.APPWRITE_PROJECT_ID)
+      .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT || process.env.APPWRITE_ENDPOINT)
+      .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID || process.env.APPWRITE_PROJECT_ID)
       .setKey(process.env.APPWRITE_API_KEY);
 
     const databases = new Databases(client);
